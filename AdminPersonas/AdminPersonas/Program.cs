de42d66a36b580b40eebe250bb9c617cfcfa5ea9
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-03-25 08:43:00 -0300

    inicial

---
+++ AdminPersonas/AdminPersonas/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  https://github.com/orionpgaete/AP-11-171  */
+
+namespace AdminPersonas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            uint telefono;
+            double peso;
+            double estatura;
+
+            Console.WriteLine("Bienvenido al SW Ultradistinto y mas BKN");
+
+            bool esValido;
+
+            do
+            {
+                Console.WriteLine("Ingrese Telefono");
+                //string tel = Console.ReadLine().Trim();
+                //esValido = UInt32.TryParse(tel, out telefono);
+                esValido = UInt32.TryParse(Console.ReadLine().Trim(), out telefono);
+            } while (!esValido);
+
+            do
+            {
+                Console.WriteLine("Ingrese Peso");
+                esValido = Double.TryParse(Console.ReadLine().Trim(), out peso);
+            } while (!esValido);
+
+            do
+            {
+                Console.WriteLine("Ingrese estatura");
+                esValido = Double.TryParse(Console.ReadLine().Trim(), out estatura);
+            } while (!esValido);
+
+            do
+            {
+                Console.WriteLine("Ingrese nombre");
+                nombre = Console.ReadLine().Trim();
+
+            } while (nombre.Equals(string.Empty));
+            //nombre == string.Empty
+
+            Console.WriteLine("Nombre : {0}", nombre);
+            Console.WriteLine("Telefono : {0}", telefono);
+            Console.WriteLine("Peso : {0}", peso);
+            Console.WriteLine("Estatura : {0}", estatura);
+            Console.WriteLine("IMC : {0}", peso / (estatura * estatura));
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ HolaMundo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Ingrese nombre");
+            string nombre = Console.ReadLine().Trim();
+            Console.WriteLine("Ingrese edad");
+            string edadTx = Console.ReadLine().Trim();
+            int edad = -1;
+            bool esValido = Int32.TryParse(edadTx, out edad);
+            if (!esValido)
+            {
+                Console.WriteLine("Ingrese bien la edad");
+            }
+            else
+            {
+                Console.WriteLine("Su nombre es : {0} su edad es {1}", nombre, edad);
+            }
+   
+/*
+            try
+            {
+                string num = "12";
+                int num2 = Convert.ToInt32(num);
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+
+            string num = "12";
+            int num2;
+            bool esValido = int.TryParse(num, out num2);
+*/
+
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
